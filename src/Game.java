e947094b14ee4d321953e816bc135a9ab5f24e2e
Ayakk <alihan26262626@gmail.com>
2020-04-02 13:10:20 +0200

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,24 @@
+public class Game {
+    private String naam;
+    private int releaseJaar;
+    private double nieuwprijs;
+
+    public Game(String nm, int rJ, double nwpr){
+
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double huidigeWaarde(){
+        return 0;
+    }
+
+    public boolean equals(Object andereObject){
+        return false;
+    }
+    public String toString(){
+        return null;
+    }
+}
